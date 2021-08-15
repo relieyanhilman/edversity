c75176296d34734515a7d5bf57caf29e5fa688ba
relieyanhilman <relieyanhilman93@gmail.com>
2021-08-15 08:34:51 +0800

    modifikasi form register

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
 
-
+//test
 app.get('/', (req, res) => {
   res.send('hallo dunia');
 })
 
+//form register
+app.get('/registerStudent', (req, res) => {
+    res.render('register-pelajar');
+})
+
+//post register
+app.post('/', async(req, res) => {
+  try{
+    
+  } catch(err){
+
+  }
+
+})
 
 
 app.listen(3000, () => {
