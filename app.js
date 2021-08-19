c92e84c6382101cd6a779401456965e5d16a5d38
relieyanhilman <relieyanhilman93@gmail.com>
2021-08-19 13:37:29 +0800

    render buka-kelas, request-kelas, fix authentikasi mentor

--- app.js
+++ app.js
@@ -56,10 +56,28 @@ app.get("/loginStudent", (req, res) => {
   res.render("student/login-pelajar");
 });
 
+//isLoggedIn? student 
+function isLoggedInStudent(req,res,next){
+  if(!req.isAuthenticated()){
+    // req.session.kembaliKe = req.originalUrl;
+    req.flash('error', 'anda harus login dulu');
+    return res.redirect('/loginStudent');
+  }
+  next();
+}
+
+
+
 //dashboard student
-app.get("/dashboardStudent", isLoggedInStudent, (req, res) => {
+app.get("/dashboardStudent", isLoggedInStudent, catchAsync(async (req, res) => {
+  // pool.query(
+  //   ``,
+  //   [req.params.id]
+  //   )  
+  // console.log(req.user);
   res.render("student/home");
-});
+}));
+
 
 
 //post register student
@@ -87,7 +105,7 @@ app.post(
         RETURNING email, password`,
         [nama_lengkap, email, hashedPassword, asal_sekolah, angkatan, jenjang]
       );
-      console.log(rowsInsert.rows[0]);
+      // console.log(rowsInsert.rows[0]);
       req.flash("success", "kamu sudah terdaftar, silakan login");
       res.redirect("/loginStudent");
     }
@@ -95,15 +113,6 @@ app.post(
 );
 
 
-//isLoggedIn? student 
-function isLoggedInStudent(req,res,next) {
-  if(!req.isAuthenticated()){
-    // req.session.kembaliKe = req.originalUrl;
-    req.flash('error', 'anda harus login dulu');
-    return res.redirect('/loginStudent');
-  }
-  next();
-}
 
 //post login student
 app.post(
@@ -115,13 +124,30 @@ app.post(
   })
 );
 
+//edpedia comingsoon
+app.get('/edpedia-comingsoon', isLoggedInStudent, (req, res) => {
+  res.render('student/edpedia');
+})
+
+app.get("/buka-kelas", (req, res) => {
+  res.render('student/bukakelas-pelajar');
+});
+
+app.get("/request-kelas", (req, res) => {
+  res.render('student/requestkelas');
+})
+
+app.post('/', catchAsync(async(req, res) => {
+
+}))
+
+
 //MENTOR
 
 //form login mentor
 app.get('/loginMentor', (req, res) => {
   res.render('mentor/login-mentor');
 })
-
 
 
 //post register mentor
@@ -168,9 +194,18 @@ app.post(
 );
 
 //dashboard mentor
-app.get('/dashboardMentor', (req, res) => {
+app.get('/dashboardMentor', isLoggedInMentor, (req, res) => {
   res.render('mentor/home-mentor');
 })
+
+//Check authentikasi mentor
+function isLoggedInMentor(req, res, next) {
+  if(!req.isAuthenticated()){
+    req.flash('error', 'anda harus login dulu');
+    return res.redirect('/loginMentor');
+  }
+  next();
+}
 
 
 app.listen(3000, () => {
--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -1,23 +1,23 @@
 const navbar = document.querySelectorAll(".navbar")
 navbar.forEach(navs=> {
   navs.innerHTML = `<div class="container-fluid">
-  <a class="navbar-brand ms-3" href="home.html"><img src="IMG/logo.svg" /></a>
+  <a class="navbar-brand ms-3" href="/dashboardStudent"><img src="IMG/logo.svg" /></a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+        <a class="nav-link active" aria-current="page" href="/dashboardStudent">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="edpedia.html">ed.pedia</a>
+        <a class="nav-link" href="/edpedia-comingsoon">ed.pedia</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="edwallet.html" tabindex="-1">ed.wallet</a>
       </li>
       <li class="nav-item">
-        <a style="color: #fff100 !important" class="nav-link" href="bukakelas-pelajar.html">
+        <a style="color: #fff100 !important" class="nav-link" href="/buka-kelas">
           BUKA KELAS
         </a>
       </li>
